580bb1733d272a603119fbd4b1dc1247ec8d9c3c
David Rodriguez <davidrodriguezcaro6@gmail.com>
2025-03-19 22:01:23 -0500

    Final Version

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Página web: 'Análisis de Datos de Anuncios de Venta de Vehículos'
 
-Podrás encontrar información detallada sobre anuncios de venta de coches, junto con una serie de gráficos explicativos que ilustran los datos recopilados.
+Podrás acceder a información detallada sobre datos de venta de vehículos, complementada con una serie de gráficos explicativos que facilitan la interpretación y análisis de los datos recopilados.
+
+La plataforma contará con una interfaz de usuario intuitiva y de fácil interacción, permitiendo aplicar filtros personalizados para visualizar datos específicos. Esto te permitirá analizar tendencias y identificar puntos estratégicos de manera clara y eficiente. 
--- app.py
+++ app.py
@@ -19,6 +19,21 @@ En este proyecto se realizara un analisis preliminar de datos del conjunto `vehi
 st.header("Exploración de datos")
 st.dataframe(car_data)
 
+st.subheader("Filtrar por año de fabricación:")
+st.markdown("""Como analista de datos, se puede realizar un filtrado de los datos utilizando la columna `model_year` para segmentar los vehículos según su año de modelo. Este enfoque permite identificar patrones de comportamiento de compra de los consumidores, analizando tendencias y preferencias asociadas a vehículos de diferentes años.""")
+
+selected_year = st.selectbox("Selecciona un año:", sorted(
+    car_data['model_year'].dropna().unique().astype(int)))
+
+# Filtrar datos por el año seleccionado
+filtered_data = car_data[car_data['model_year'] == selected_year]
+
+# Mostrar la tabla filtrada
+st.dataframe(filtered_data)
+
+st.header("Distribucion Grafica basado en el Odometro de los Vehiculos")
+st.markdown(""" - Se puede seleccionar y visualizar la distribución gráfica de los precios de los vehículos en función de su kilometraje (odómetro), lo que permitirá identificar patrones, tendencias y relaciones entre estas dos variables clave.""")
+
 # Botones para mostrar/ocultar gráficos
 show_hist = st.checkbox(" Mostrar Histograma", value=True)
 show_scatter = st.checkbox("Mostrar Gráfico de Dispersión", value=True)
@@ -45,3 +60,6 @@ if show_scatter:
             title="Relación entre Odómetro y Precio"
         )
         st.plotly_chart(fig_scatter, use_container_width=True)
+
+st.header("Next Analysis")
+st.subheader("To be Continue")
